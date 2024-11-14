bb1af1be6e0632bd389c088fe17286ccc1bf0245
sitebobines <165167077+sitebobines@users.noreply.github.com>
2024-11-14 17:52:40 +0100

    Add files via upload

---
+++ New Panorama3.js
@@ -0,0 +1,121 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#ffffff; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 100; text-align:left; box-shadow: 4px 4px 1px 0px rgba(0,0,0,0.33);background: rgba(0,0,0,0.66);"
+    },
+"include": [
+        {
+            "src": "New Panorama3_controls.js"
+        }
+    ],
+"node": {
+	"autoplay": {
+	        "pan": 0.08,
+	        "reference": "screen",
+	        "restart": 5,
+	        "returnToLevel": true,
+	        "status": "off",
+	        "tilt": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 44.06250000000006,
+	        "maxtilt": 10.530827557338403,
+	        "minpan": 0,
+	        "mintilt": -10.76484594750148
+	    },
+	"hotspot": [
+	        {
+	            "bitmap": "facebook-picto.png",
+	            "bitmaptype": "embedded",
+	            "embeddedwidth": 1.0948467452544732,
+	            "href": "javascript:",
+	            "hsinfo": {
+	                "align": "center",
+	                "captureMouse": true,
+	                "checkHover": true,
+	                "id": "hsinfo0",
+	                "maxwidth": "66%",
+	                "onclick": function(){ this.viewer.action('hideMessage'); } ,
+	                "style": "min-width: 25%; text-shadow: none; line-height: 100%; color:#ffffff; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 100; text-align:left; box-shadow: 4px 4px 1px 0px rgba(0,0,0,0.33);background: rgba(0,0,0,0.66); transform-origin : 0% 0% 0px; transform: translate(0px, 0px) translateZ(10000px);",
+	                "text": "<div style='padding: 16px;' class='panoStudioViewerText1'><style>div.panoStudioViewerText1 a, a:visited {color: #FDB815;} div.panoStudioViewerText1 a:hover, a:active {color: #EFF517;}</style><img src=\"https://cdn1.regie-agricole.com/ulf/CMS_Content/2/articles/798104/LH107901701_1-1000x562.jpg\" alt=\"Beau gosse\"/></div>",
+	                "xoff": 0,
+	                "yoff": -30
+	            },
+	            "onclick": function(){ this.viewer.action('toggleMessage',this.hsinfo); },
+	            "opacity": "0.85;1.00",
+	            "position": "12.9419,-3.2734",
+	            "rotate": 0,
+	            "textbox": {
+	                "align": "top",
+	                "style": "color:#ffffff; font-family: Arial, Helvetica, sans-serif; font-size: 12px; font-weight: normal; text-align:center; display: inline;",
+	                "text": "BEAU GOSSE\n"
+	            },
+	            "unit": "deg"
+	        },
+	        {
+	            "bitmap": "autresgaronnes.gif",
+	            "bitmaptype": "embedded",
+	            "embeddedwidth": 4.406250065658247,
+	            "href": "https://www.lesbobinesdupaysage.com/autresgaronnes",
+	            "opacity": "0.50;1.00",
+	            "position": "33.8577,-6.3665",
+	            "rotate": 0,
+	            "target": "",
+	            "textbox": {
+	                "align": "top",
+	                "style": "color:#ffffff; font-family: Arial, Helvetica, sans-serif; font-size: 12px; font-weight: normal; text-align:center; display: inline;",
+	                "text": "Cette mission est un travail entamé en 2018 dans la vallée du Comminges, puis se déroulant chaque année sur une grande entité paysagère de la Garonne de Haute Garonne. En 2019, dans le territoire du Volvestre; en 2020, dans la métropole toulousaine; 2021, la Garonne pyrénéenne; 2022, la Garonne Muretaine et pour terminer 2023 (carnet en cours ...) pour la séquence Nord de Toulouse."
+	            },
+	            "transition": "zoomin,2",
+	            "unit": "deg"
+	        },
+	        {
+	            "bitmap": "pap_info_white.png",
+	            "bitmaptype": "embedded",
+	            "embeddedwidth": 1.448634361149745,
+	            "href": "javascript:",
+	            "hsinfo": {
+	                "align": "center",
+	                "captureMouse": true,
+	                "checkHover": true,
+	                "id": "hsinfo2",
+	                "maxwidth": "66%",
+	                "onclick": function(){ this.viewer.action('hideMessage'); } ,
+	                "style": "min-width: 25%; text-shadow: none; line-height: 100%; color:#ffffff; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 100; text-align:left; box-shadow: 4px 4px 1px 0px rgba(0,0,0,0.33);background: rgba(0,0,0,0.66); transform-origin : 0% 0% 0px; transform: translate(0px, 0px) translateZ(10000px);",
+	                "text": "<div style='padding: 16px;' class='panoStudioViewerText1'><style>div.panoStudioViewerText1 a, a:visited {color: #FDB815;} div.panoStudioViewerText1 a:hover, a:active {color: #EFF517;}</style><img src=\"https://media.sudouest.fr/7506321/1200x-1/medoc-ea414136fb5f494d8247d72105391ad6-094418-ph0.jpg\n\" alt=\"AIE AIE AIE\"/></div>",
+	                "xoff": 0,
+	                "yoff": -30
+	            },
+	            "onclick": function(){ this.viewer.action('toggleMessage',this.hsinfo); },
+	            "opacity": "0.00;0.00",
+	            "position": "32.2914,-0.3543",
+	            "rotate": 0.386993408203125,
+	            "textbox": {
+	                "align": "top",
+	                "style": "color:#ffffff; font-family: Arial, Helvetica, sans-serif; font-size: 12px; font-weight: normal; text-align:center; display: inline;",
+	                "text": "SI SIiiiii !!!! "
+	            },
+	            "unit": "deg"
+	        }
+	    ],
+	"hotspots": {
+	        "visible": true
+	    },
+	"id": "New Panorama3",
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"New Panorama3_tiles/New Panorama3_t_1b_%0y_%0x.jpg"},"down":{"src":"New Panorama3_tiles/New Panorama3_t_1d_%0y_%0x.jpg"},"front":{"src":"New Panorama3_tiles/New Panorama3_t_1f_%0y_%0x.jpg"},"height":14485,"left":{"src":"New Panorama3_tiles/New Panorama3_t_1l_%0y_%0x.jpg"},"right":{"src":"New Panorama3_tiles/New Panorama3_t_1r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"New Panorama3_tiles/New Panorama3_t_1u_%0y_%0x.jpg"},"width":14485},{"back":{"src":"New Panorama3_tiles/New Panorama3_t_2b_%0y_%0x.jpg"},"down":{"src":"New Panorama3_tiles/New Panorama3_t_2d_%0y_%0x.jpg"},"front":{"src":"New Panorama3_tiles/New Panorama3_t_2f_%0y_%0x.jpg"},"height":7243,"left":{"src":"New Panorama3_tiles/New Panorama3_t_2l_%0y_%0x.jpg"},"right":{"src":"New Panorama3_tiles/New Panorama3_t_2r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"New Panorama3_tiles/New Panorama3_t_2u_%0y_%0x.jpg"},"width":7243},{"back":{"src":"New Panorama3_tiles/New Panorama3_t_3b_%0y_%0x.jpg"},"down":{"src":"New Panorama3_tiles/New Panorama3_t_3d_%0y_%0x.jpg"},"front":{"src":"New Panorama3_tiles/New Panorama3_t_3f_%0y_%0x.jpg"},"height":3622,"left":{"src":"New Panorama3_tiles/New Panorama3_t_3l_%0y_%0x.jpg"},"right":{"src":"New Panorama3_tiles/New Panorama3_t_3r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"New Panorama3_tiles/New Panorama3_t_3u_%0y_%0x.jpg"},"width":3622},{"back":{"src":"New Panorama3_tiles/New Panorama3_t_4b_%0y_%0x.jpg"},"down":{"src":"New Panorama3_tiles/New Panorama3_t_4d_%0y_%0x.jpg"},"front":{"src":"New Panorama3_tiles/New Panorama3_t_4f_%0y_%0x.jpg"},"height":1811,"left":{"src":"New Panorama3_tiles/New Panorama3_t_4l_%0y_%0x.jpg"},"right":{"src":"New Panorama3_tiles/New Panorama3_t_4r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"New Panorama3_tiles/New Panorama3_t_4u_%0y_%0x.jpg"},"width":1811},{"back":{"src":"New Panorama3_tiles/New Panorama3_t_5b_%0y_%0x.jpg"},"down":{"src":"New Panorama3_tiles/New Panorama3_t_5d_%0y_%0x.jpg"},"front":{"src":"New Panorama3_tiles/New Panorama3_t_5f_%0y_%0x.jpg"},"height":906,"left":{"src":"New Panorama3_tiles/New Panorama3_t_5l_%0y_%0x.jpg"},"right":{"src":"New Panorama3_tiles/New Panorama3_t_5r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"New Panorama3_tiles/New Panorama3_t_5u_%0y_%0x.jpg"},"width":906},{"back":{"src":"New Panorama3_tiles/New Panorama3_t_6b_%0y_%0x.jpg"},"down":{"src":"New Panorama3_tiles/New Panorama3_t_6d_%0y_%0x.jpg"},"front":{"src":"New Panorama3_tiles/New Panorama3_t_6f_%0y_%0x.jpg"},"height":453,"left":{"src":"New Panorama3_tiles/New Panorama3_t_6l_%0y_%0x.jpg"},"right":{"src":"New Panorama3_tiles/New Panorama3_t_6r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"New Panorama3_tiles/New Panorama3_t_6u_%0y_%0x.jpg"},"width":453}],"multilevel":true,"preview":{"src":"New Panorama3_tiles/New Panorama3_preview.jpg"},"projection":"cubic"},
+	"view": {
+	        "hfov": 31.94,
+	        "pan": 22.03,
+	        "tilt": 0
+	    }
+},
+"version": "4.2"
+}
+}
